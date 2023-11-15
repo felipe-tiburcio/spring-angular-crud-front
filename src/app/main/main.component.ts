88c074bc7031b278fb18c9e79c52b20a23ef707b
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2023-11-14 22:21:04 -0300

    buttons visibility

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent {
-
+  isVisible: boolean = true;
 }
